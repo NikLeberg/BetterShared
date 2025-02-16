417aadc060b740ac2c48f8f8f22b341ded619337
NikLeberg <niklaus.leuenb+github@gmail.com>
2025-02-15 22:33:39 -0800

    add count

--- app/src/main/java/ch/nikleberg/bettershared/data/Folder.java
+++ app/src/main/java/ch/nikleberg/bettershared/data/Folder.java
@@ -8,15 +8,18 @@ public class Folder {
     public String id;
 
     public String name;
+    public int count;
 
-    public Folder(String id, String name) {
+    public Folder(String id, String name, int count) {
         this.id = id;
         this.name = name;
+        this.count = count;
     }
 
     public boolean equals(@NonNull Folder folder) {
         return (id.equals(folder.id) &&
-                name.equals(folder.name)
+                name.equals(folder.name) &&
+                count == folder.count
         );
     }
 }
